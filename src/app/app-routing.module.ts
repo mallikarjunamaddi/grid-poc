a7396aa20ff9ab5b16b24d1df9aea2bf93ea7e09
Mallikarjuna Maddi <mmaddi@microsoft.com>
2022-08-24 13:51:11 +0530

    Added route for grid component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GridComponent } from './grid/grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'grid', component: GridComponent, title: 'grid' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
