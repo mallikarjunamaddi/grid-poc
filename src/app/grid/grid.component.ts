97227ee73f7decb466fe3251471b61fddd430e5a
Mallikarjuna Maddi <mmaddi@microsoft.com>
2022-08-26 10:54:43 +0530

    Added generic grid comonent

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
-  constructor() { }
+  gridTitle: string;
+  rows: Array<any>;
+  Id: string = 'Id'
+  columns: Array<GridColumn>;
+  constructor() {
+    this.gridTitle = "Activity Type";
+    this.rows = [
+      { Id: 545, Value: '1-Initial Asesmnt and prep-2wek iter Srv', Code: 'ESSBNH', ActivityTypeCategoryName: 'Service Based', IsCostRateRegistrationAllowed: true, Edit: 'Edit', Remove: 'Remove' },
+      { Id: 500, Value: '1-Initial Assess and prep-2week iter Srv', Code: 'ESSBNK', ActivityTypeCategoryName: 'Service Based', IsCostRateRegistrationAllowed: true, Edit: 'Edit', Remove: 'Remove' },
+    ];
+    this.columns = [
+      { PropertyName: 'Id', Title: 'Activity Type Id', Class: 'codeCol' },
+      { PropertyName: 'Value', Title: 'Activity Type Name' },
+      { PropertyName: 'Code', Title: 'Code', Class: 'actionCol' },
+      { PropertyName: 'ActivityTypeCategoryName', Title: 'Activity Type Category', Class: 'codeCol' },
+      { PropertyName: 'IsCostRateRegistrationAllowed', Title: 'Is Cost Rate Registration Allowed', Class: 'codeCol' },
+      { PropertyName: 'Edit', Title: 'Edit', Class: 'actionCol' },
+      { PropertyName: 'Remove', Title: 'Remove', Class: 'actionCol' },
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+
+
+}
+
+export class GridColumn {
+  PropertyName!: string;
+  Title!: string;
+  Class?: string;
+  constructor() {
+    this.Class = '';
+  }
 }
