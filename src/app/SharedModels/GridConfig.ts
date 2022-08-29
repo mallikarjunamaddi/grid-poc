af340b8692af1f8f01fd3dc901c13dbc6af13a6f
Mallikarjuna Maddi <mmaddi@microsoft.com>
2022-08-29 17:20:33 +0530

    Added comments.

--- src/app/SharedModels/GridConfig.ts
+++ src/app/SharedModels/GridConfig.ts
@@ -1,15 +1,15 @@
 export class GridConfig {
-    GridTitle!: string;
-    IsActionsAllowed!: boolean;
-    IsActionsEnabled!: boolean;
-    Rows!: Array<any>;
-    Columns!: Array<GridColumn>;
-    RowProperty!: string;
+    GridTitle!: string; //Gird Summary
+    IsActionsAllowed!: boolean; //Edit and Remove Actions
+    IsActionsEnabled!: boolean; //To enable or disable the Actions.
+    Rows!: Array<any>; //Data  Rows
+    Columns!: Array<GridColumn>; //Columns List
+    RowProperty!: string; //Primary Key item in the columns
 }
 export class GridColumn {
-    PropertyName!: string;
-    Title!: string;
-    Class?: string;
+    PropertyName!: string; //Json Property name
+    Title!: string; //Column header
+    Class?: string; //CSS Class
     constructor() {
         this.Class = '';
     }
